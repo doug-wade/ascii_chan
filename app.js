2c81ac46929631121837d0eecbd15cc0b19a302a
Doug Wade <prekolna@gmail.com>
2013-07-18 21:08:03 -0500

    configured route and view for / and index

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    routes = require('./routes');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 8080);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(app.router);
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});

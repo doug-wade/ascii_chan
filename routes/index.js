a42f7f40b7b3b4f3dfa9f752b2daef76e1dae190
Doug Wade <prekolna@gmail.com>
2013-07-18 23:17:23 -0500

    form retains values on failure.

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,21 @@ exports.post = function(req, res){
   var error;
   var title = req.body.title;
   var art = req.body.art;
-  console.log(req.body);
   if (!title){
     if (!art){
       error = 'Please provide a title and some art.'
     } else {
       error = 'Please provide a title.';
     }
+    res.render('index', {page_title: 'ascii', error: error, title: title, art: art});
   } else if (!art){
     error = 'Please provide some ascii art.';
+    res.render('index', {page_title: 'ascii', error: error, title: title, art: art});
   }
-  res.render('index', {title: 'ascii', error: error});
+  res.render('thanks', {page_title: 'ascii'});
   return;
+}
+
+exports.thanks = function(req, res){
+  res.render('thanks', {page_title:'thanks'});
 }

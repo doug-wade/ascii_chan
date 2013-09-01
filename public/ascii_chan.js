0382015a262395ebea8b056a28689252d1cf8767
prekolna <prekolna@gmail.com>
2013-09-01 16:35:37 -0500

    Fix gradient on thanks page

--- public/ascii_chan.js
+++ public/ascii_chan.js
@@ -1,4 +1,13 @@
+function adjustGradient(){
+  var h = $(window).height(),
+      w = $(window).width();
+
+  $('.container').css({'min-height': h,'min-width': w});
+}
+
 $(document).ready(function(){
+  adjustGradient();
+
   if (geo_position_js.init()){
     geo_position_js.getCurrentPosition(function(p){
       $('.geoPosition').val(p.coords.latitude + "|" + p.coords.longitude);
@@ -6,4 +15,6 @@ $(document).ready(function(){
       console.log("fail.");
     });
   }
+
+  $(window).resize(adjustGradient);
 })
